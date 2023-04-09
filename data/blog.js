a88e313467741682005bfbc4b7030b76fe679dc7
himasnhuJasani <himanshujasani21@gmail.com>
2023-04-09 10:45:18 +0530

    multikart-nuxt3

---
+++ data/blog.js
@@ -0,0 +1,490 @@
+export const BlogData = [
+    {
+        type: "fashion",
+        img: '/images/blog/1.jpg',
+        link: '/blog/details',
+        title: '25 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "fashion",
+        img: '/images/blog/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "fashion",
+        img: '/images/blog/3.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "fashion",
+        img: '/images/blog/4.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "fashion",
+        img: '/images/blog/5.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "fashion",
+        img: '/images/blog/6.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "beauty",
+        img: '/images/beauty/blog/1.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "beauty",
+        img: '/images/beauty/blog/2.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "beauty",
+        img: '/images/beauty/blog/3.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "beauty",
+        img: '/images/beauty/blog/4.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment',
+        shortDesc: 'you how all this mistaken idea of denouncing pleasure and praising pain was born',
+        longDesc: 'Consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.'
+    },
+    {
+        type: "pets",
+        img: '/images/pets/blog/1.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "pets",
+        img: '/images/pets/blog/2.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "pets",
+        img: '/images/pets/blog/3.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "pets",
+        img: '/images/pets/blog/4.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "bags",
+        img: '/images/blog/layout3/1.jpg',
+        link: '/blog/details',
+        title: '25 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "bags",
+        img: '/images/blog/layout3/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "bags",
+        img: '/images/blog/layout3/3.jpg',
+        link: '/blog/details',
+        title: '29 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "bags",
+        img: '/images/blog/layout3/4.jpg',
+        link: '/blog/details',
+        title: '25 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "bags",
+        img: '/images/blog/layout3/5.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "flower",
+        img: '/images/flower/blog/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    }, {
+        type: "flower",
+        img: '/images/flower/blog/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "flower",
+        img: '/images/flower/blog/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "furniture",
+        img: '/images/furniture/blog1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "furniture",
+        img: '/images/furniture/blog2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "furniture",
+        img: '/images/furniture/blog3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "furniture",
+        img: '/images/furniture/selk.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "vegetables",
+        img: '/images/vegetables/blog/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    }, {
+        type: "vegetables",
+        img: '/images/vegetables/blog/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    }, {
+        type: "vegetables",
+        img: '/images/vegetables/blog/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    }, {
+        type: "vegetables",
+        img: '/images/vegetables/blog/4.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "shoes",
+        img: '/images/blog/layout2/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "shoes",
+        img: '/images/blog/layout2/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "shoes",
+        img: '/images/blog/layout2/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "shoes",
+        img: '/images/blog/layout2/4.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "shoes",
+        img: '/images/blog/layout2/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "nursery",
+        img: '/images/nursery/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "nursery",
+        img: '/images/nursery/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "nursery",
+        img: '/images/nursery/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "gym",
+        img: '/images/gym/blog/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "gym",
+        img: '/images/gym/blog/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "gym",
+        img: '/images/gym/blog/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "gym",
+        img: '/images/gym/blog/4.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "gym",
+        img: '/images/gym/blog/5.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "marijuana",
+        img: '/images/marijuana/blog/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "marijuana",
+        img: '/images/marijuana/blog/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "marijuana",
+        img: '/images/marijuana/blog/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "watch",
+        img: '/images/blog/layout4/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "watch",
+        img: '/images/blog/layout4/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "watch",
+        img: '/images/blog/layout4/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "watch",
+        img: '/images/blog/layout4/4.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "watch",
+        img: '/images/blog/layout4/5.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/1.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/7.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/2.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/3.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/4.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/5.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/6.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    },
+    {
+        type: "christmas",
+        img: '/images/christmas/blog/7.jpg',
+        link: '/blog/details',
+        title: '26 January 2022',
+        desc: 'Lorem ipsum dolor sit consectetur adipiscing elit',
+        date: 'by: John Dio , 2 Comment'
+    }
+]
